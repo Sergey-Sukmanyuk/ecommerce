e41c3e94b681bbaaa2dc450695dd71139532990c
Serhii <ssukmanyuk@gmail.com>
2024-03-07 15:08:04 +0200

    Ininita commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/"> eCommerce </a>
+      </header>
+      <main>
+        Product list
+      </main>
+    </div>
+  );
+}
+
+export default App;
